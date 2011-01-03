eda2c9ea45a302eb90084338485820d0a85e7268
K. Richard Pixley <rich@noir.com>
2011-01-02 18:34:47 -0800

    first pass

--- objdump.py
+++ objdump.py
@@ -4,7 +4,7 @@
 # Copyright 2010 K. Richard Pixley.
 # See LICENSE for details.
 #
-# Time-stamp: <02-Jan-2011 15:15:56 PST by rich@noir.com>
+# Time-stamp: <02-Jan-2011 18:27:10 PST by rich@noir.com>
 
 """
 A covering script for :py:mod:`elffile`.  Provides basic objdump ability.
@@ -14,7 +14,11 @@ from __future__ import unicode_literals, print_function
 
 __docformat__ = 'restructuredtext en'
 
+import glob
+import itertools
 import optparse
+import pprint
+import sys
 
 import elffile
 
@@ -28,7 +32,7 @@ if __name__ == '__main__':
 
     options, args = parser.parse_args()
 
-    for i in args:
-        elffile.open(name=i).dump()
+    for i in reduce(itertools.chain, (glob.iglob(arg) for arg in args)):
+        pprint.pprint(elffile.open(name=i)._list_encode(), sys.stdout, 1, 202)
 
     sys.exit()
