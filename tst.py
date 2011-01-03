d340fa21c71a7f5549f4eca27f21ddf68dc6ccf9
K. Richard Pixley <rich@noir.com>
2011-01-02 19:20:16 -0800

    ad hoc test file

---
+++ tst.py
@@ -0,0 +1,27 @@
+import yaml
+import elffile
+import sys
+import glob
+import os
+import itertools
+
+def filelist(which):
+    for f in glob.iglob(os.path.join('testfiles', which, '.libs', 'hello')):
+        yield f
+
+    for f in glob.iglob(os.path.join('testfiles', which, '.libs', '*.so*')):
+        yield f
+
+    for f in glob.iglob(os.path.join('testfiles', which, '.libs', '*.o')):
+        yield f
+
+    for f in glob.iglob(os.path.join('testfiles', which, '*.o')):
+        yield f
+
+for xname, yname in itertools.izip(filelist('one'), filelist('two')):
+
+    x = elffile.open(name=xname)
+    y = elffile.open(name=yname)
+
+    print('{0}: {1}'.format(xname, x == y))
+
